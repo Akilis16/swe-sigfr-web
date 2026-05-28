29fa3a96f5941a6264cf506db2b4882915b248c5
Akilis16 <gabrielsebastiao16072005@gmail.com>
2026-05-28 00:29:00 -0300

    feat(core): configures HTTP client, React query cache, and authentication store

---
+++ src/lib/queryClient.ts
@@ -0,0 +1,11 @@
+import { QueryClient } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 0,
+      staleTime: 0,
+      refetchOnWindowFocus: true,
+    },
+  },
+});
